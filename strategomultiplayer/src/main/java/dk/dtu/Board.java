486a4f7299476143c88c2086c93624e102de735a
Zwinge <zwingechristopher@hotmail.com>
2023-01-11 12:13:05 +0100

    Added way to set initial game piece postions

--- strategomultiplayer/src/main/java/dk/dtu/Board.java
+++ strategomultiplayer/src/main/java/dk/dtu/Board.java
@@ -1,13 +1,15 @@
 package dk.dtu;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.jspace.SequentialSpace;
 import org.jspace.Space;
-import org.jspace.Tuple;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -48,6 +50,42 @@ public class Board {
     }
     
     public static void initGamePieces(){
+    	String defaultString = "S112344556";
+    	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    	System.out.println("Input initial setup of pieces as 40 character string");
+    	String string = null;
+    	try {
+			string = input.readLine();
+			if(string.length()>10) {
+				System.out.println("String is too long");
+				string = null;
+			} else if(string.length() <10){
+				System.out.println("String is shorter than maximum using partial piece placements");
+				string = null;
+			} else {
+				if(string.matches("^[SBF0-9]+$")) {
+					System.out.println("String works");
+				} else {
+					System.out.println("String uses characters other than B F S 0 1 2 3 4 5 6 7 8 9");
+				}
+			}
+			
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    	if(string == null) {
+    		string = defaultString;
+    	}
+    	
+    	for(int row = 0; row < 4; row++) {
+    		for(int col = 0; col<10; col++) {
+    			
+    		}
+    	}
+    	
+    	
+    	
         ArrayList<Tile> AllTiles = new ArrayList<>();
         for(Tile[] tileRow : tiles){
             AllTiles.addAll(Arrays.asList(tileRow));
@@ -73,6 +111,27 @@ public class Board {
         }
         
         
+    }
+    
+    
+    public static PieceType pieceSwitch(char c) {
+    	switch(c) {
+    		case 'S': return PieceType.SPY;
+    		case 'F': return PieceType.FLAG;
+    		case 'B': return PieceType.BOMB;
+    		case '2': return PieceType.SCOUT;
+    		case '3': return PieceType.MINER;
+    		case '4': return PieceType.SERGEANT;
+    		case '5': return PieceType.LIEUTENANT;
+    		case '6': return PieceType.CAPTAIN;
+    		case '7': return PieceType.MAJOR;
+    		case '8': return PieceType.COLONEL;
+    		case '9': return PieceType.GENERAL;
+    		case '0': return PieceType.MARSHAL;
+    	}
+    	
+    	
+    	return PieceType.FLAG;
     }
 
 
